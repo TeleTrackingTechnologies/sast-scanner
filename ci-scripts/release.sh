f332ff7c23967444bca4dc974902e75b4524a029
Clint Wadley <clinton-wadley@teletracking.com>
2020-09-02 16:43:06 -0400

    Tools 380 (#10)
    * Gotta install semversioner for the release script
    
    * Now I gotta make sure that python 3.7 is installed
    
    * Turns out, we actually need to do it all within the same job, because we would have to rebuild the images otherwise, as they don't carry over
    
    * Tagging images with teletracking org

--- ci-scripts/release.sh
+++ ci-scripts/release.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 IMAGE=$(echo $1 | tr [A-Z] [a-z])
 VERSION=$(semversioner current-version)
 
-docker push "${IMAGE}/bb-pipe:latest"
-docker push "${IMAGE}/bb-pipe:${VERSION}"
+docker push "teletracking/${IMAGE}/bb-pipe:latest"
+docker push "teletracking/${IMAGE}/bb-pipe:${VERSION}"
 
-docker push "${IMAGE}/cci-orb:latest"
-docker push "${IMAGE}/cci-orb:${VERSION}"
+docker push "teletracking/${IMAGE}/cci-orb:latest"
+docker push "teletracking/${IMAGE}/cci-orb:${VERSION}"
