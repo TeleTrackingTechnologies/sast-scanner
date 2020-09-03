d6d55052aa1089c0059542956bf142269e078709
Clinton Wadley <clinton-wadley@teletracking.com>
2020-09-02 22:20:45 -0400

    Docker hub doesn't allow more than one path segment after the organization :(

--- ci-scripts/release.sh
+++ ci-scripts/release.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 IMAGE="teletracking/$(echo $1 | tr [A-Z] [a-z] | xargs basename)"
 VERSION=$(semversioner current-version)
 
-docker push "${IMAGE}/bb-pipe:latest"
-docker push "${IMAGE}/bb-pipe:${VERSION}"
+docker push "${IMAGE}-bb-pipe:latest"
+docker push "${IMAGE}-bb-pipe:${VERSION}"
 
-docker push "${IMAGE}/cci-orb:latest"
-docker push "${IMAGE}/cci-orb:${VERSION}"
+docker push "${IMAGE}-cci-orb:latest"
+docker push "${IMAGE}-cci-orb:${VERSION}"
